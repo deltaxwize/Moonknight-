434ba8df975bee71383b7a1872e5cf634d476fd0
psysinner <psychosinner@proton.me>
2026-02-27 01:18:10 +0530

    link

--- plugins/start.py
+++ plugins/start.py
@@ -69,7 +69,7 @@ async def start_command(client: Client, message: Message):
                         InlineKeyboardButton("ᴛᴜᴛᴏʀɪᴀʟ •", url=tutorial_link)
                     ],
                     [
-                        InlineKeyboardButton(" • ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ •", url="https://t.me/Premium_Fliix/21")
+                        InlineKeyboardButton(" • мσяє вσтѕ •", url="https://t.me/PrimeTgBots")
                     ]
                 ])
             )
